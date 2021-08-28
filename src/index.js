d264499e838812c0145563848e35b13391eb227e
Your Name <jayasoorya@crayond.co>
2021-08-28 22:34:38 +0530

    without cra basic setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//https://dev.to/riddhiagrawal001/create-react-app-without-create-react-app-2lgd
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App/>,document.querySelector("#root"));
